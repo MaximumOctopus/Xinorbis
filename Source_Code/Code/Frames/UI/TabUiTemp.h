1739a66ccc5eb7b0ad91306092baab65b60da1dc
MaximumOctopus <40474413+MaximumOctopus@users.noreply.github.com>
2026-08-31 01:13:52 +0100

    2026/08/31 update

---
+++ Source_Code/Code/Frames/UI/TabUiTemp.h
@@ -0,0 +1,24 @@
+// =====================================================================
+//
+//   Xinorbis 10.0
+//
+// (c) Paul Alan Freshney 2002-2026
+//
+// paul@freshney.org
+//
+// https://github.com/MaximumOctopus/Xinorbis
+//
+// =====================================================================
+
+#pragma once
+
+#include <VCLTee.Chart.hpp>
+#include <Vcl.Grids.hpp>
+
+
+namespace TabUiTemp
+{
+	void Chart(TChart*, int, bool);
+
+	std::wstring Table(TStringGrid*, int);
+}
